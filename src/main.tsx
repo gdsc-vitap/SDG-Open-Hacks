1abce0d034b409e6bd2c8d1e1d3d0b8545adbb27
Akhil <akhilesh.tulluri@gmail.com>
2023-03-10 23:03:03 +0530

    feat(schedule): add schedule page

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,8 @@ const darkTheme = createTheme({
     colors: {
       primary: "#4285f4",
       secondary: "#DB4437",
+      gradient:
+        "linear-gradient(90deg, rgba(219,68,55,1) 30%, rgba(66,133,244,1) 100%)",
     },
   },
 });
--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -1,7 +1,31 @@
 import Heading from "../components/Heading";
-import { Text } from "@nextui-org/react";
+import { Button, Container, Text } from "@nextui-org/react";
+import { useState } from "react";
 
 const Schedule = () => {
+  const [selectedDay, setSelectedDay] = useState<number>(1);
+
+  const day1Schedule = {
+    "9:00 AM": "Opening Ceremony",
+    "9:30 AM": "Icebreaker Hour - Hacker Hangouts and Speed friending",
+    "10:00 - 10:25 AM":
+      "Empowering Change: Voices for Social Impact - Keynote Address",
+    "11:00 AM": "Hacking period begins!",
+    "1:00 PM": "Fuel Up: Time for Lunch!",
+    "2:00 PM": "Ideation Checkpoint: Refine Your Ideas",
+    "5:00 PM": "Design Checkpoint",
+  };
+
+  const day2Schedule = {
+    "9:00 AM": "Meme Design Contest: Get Creative with Visual Humor",
+    "12:00 PM": "Replit Speaker Session",
+    "2:00 PM": "Final MVP Checkpoint: The Finish Line is in Sight",
+    "3:00 PM": "Judging Period",
+    "4:00 PM": "Closing Ceremony and Awards",
+  };
+
+  const chosenDay = selectedDay === 1 ? day1Schedule : day2Schedule;
+
   return (
     <div
       style={{
@@ -14,7 +38,84 @@ const Schedule = () => {
       }}
     >
       <Heading foregroundText="Schedule" backgroundText="SCHEDULE" />
-      <Text h3>Coming Soon.</Text>
+      <Button.Group
+        color="gradient"
+        size="lg"
+        ghost
+        css={{
+          margin: "$md auto",
+        }}
+      >
+        <Button
+          css={{
+            background: selectedDay === 1 ? "$gradient" : "",
+          }}
+          onClick={() => setSelectedDay(1)}
+        >
+          12th March
+        </Button>
+        <Button
+          css={{
+            background: selectedDay === 2 ? "$gradient" : "",
+          }}
+          onClick={() => setSelectedDay(2)}
+        >
+          13th March
+        </Button>
+      </Button.Group>
+      <div
+        style={{
+          margin: "1rem auto 0 auto",
+        }}
+      >
+        <Text h2>{selectedDay === 1 ? "12th March" : "13th March"}</Text>
+      </div>
+      <div
+        style={{
+          margin: "0 auto 2rem auto",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            flexDirection: "column",
+            borderLeft: "5px solid #F4B400",
+            padding: "2rem 0 2rem 1.5rem",
+            rowGap: ".75rem",
+          }}
+        >
+          {Object.entries(chosenDay).map(([time, event]) => (
+            <div
+              style={{
+                display: "flex",
+                columnGap: "1rem",
+              }}
+            >
+              <Text
+                h4
+                color="#4285F4"
+                css={{
+                  flex: "0 0 10rem",
+                  "@smMax": {
+                    flex: "0 0 6rem",
+                  },
+                }}
+              >
+                {time}
+              </Text>
+              <Text
+                h4
+                css={{
+                  flex: "1 1 0",
+                }}
+              >
+                {event}
+              </Text>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
